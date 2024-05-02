2d8ca528aec872945bb000b3cf7c2a0bfe6c6705
Paras Chauhan <paras.chauhan@excellentwebworld.in>
2024-05-02 14:35:45 +0530

    - issue solve

--- app/src/main/java/com/eww/countrycodepicker/MainActivity.kt
+++ app/src/main/java/com/eww/countrycodepicker/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.eww.countrycodepicker.libUi.CountryCodePicker
 import com.eww.countrycodepicker.ui.theme.CountryCodePickerTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,6 +36,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+
 }
 
 @Preview(showBackground = true)
